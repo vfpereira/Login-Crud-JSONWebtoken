cb075c92fd7dc65d968c56baa9fc61a1ace561d6
vagner.franco <vagner.franco@technobox.com.br>
2020-12-08 20:12:45 -0300

    Add Hooks

--- client/src/app.js
+++ client/src/app.js
@@ -1,84 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'styles/app.css'
 import 'styles/util.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Login from 'components/Login'
 import Register from 'components/Register'
 import axios from 'axios'
-export default class App extends React.Component {
-  constructor (props) {
-    super()
 
-    this.state = {
-      auth: false,
-      token: '',
-      register: {
-        email: '',
-        password: '',
-        error: '',
-        validation: []
-      },
-      login: {
-        email: '',
-        password: ''
-      }
+export default function App () {
+  const [auth, setAuth] = useState(false)
+  const [token, setToken] = useState('')
+  const [register, setRegister] = useState({
+    email: '',
+    password: '',
+    error: '',
+    validation: []
+  })
 
-    }
+  const [login, SetLogin] = useState({
+    email: '',
+    password: ''
+  })
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleRegisterSubmit = this.handleRegisterSubmit.bind(this)
-    this.handleLoginSubmit = this.handleLoginSubmit.bind(this)
-    this.authorization = this.authorization.bind(this)
-  }
-
-  authorization (token) {
-    const config = {
-      headers: {
-        Authorization: 'Bearer ' + token,
-        'Content-Type': 'application/json',
-        Accept: 'application/json'
-      }
-    }
-    axios.post('/api/v1.0/authorization', {}, config).then(resp => {
-      this.setState({
-        auth: resp.data.auth
-      })
-    }).catch(err => {
-      console.log(err)
-      this.setState({
-        auth: false
-      })
-    })
-  }
-
-  handleChange (event, type) {
-    const targetName = event.target.name
-    const targetValue = event.target.value
+  function handleChange (e, type) {
+    const targetName = e.target.name
+    const targetValue = e.target.value
 
     if (type === 'register') {
-      this.setState(prevState => ({
-        register: {
-          ...prevState.register,
-          [targetName]: targetValue
-        }
-      }))
+      setRegister({ ...register, [targetName]: targetValue })
     }
 
     if (type === 'login') {
-      this.setState(prevState => ({
-        login: {
-          ...prevState.login,
-          [targetName]: targetValue
-        }
-      }))
+      SetLogin({ ...login, [targetName]: targetValue })
     }
   }
 
-  handleLoginSubmit (event) {
-    event.preventDefault()
+  function handleRegisterSubmit (e) {
+    e.preventDefault()
+    if (validate()) {
+      axios.post('/api/v1.0/register-user', {
+        email: register.email,
+        password: register.password
+      })
+        .then(resp => console.log(resp))
+        .catch(err => console.log(err))
+    }
+  }
+
+  function handleLoginSubmit (e) {
+    e.preventDefault()
     axios.post('/api/v1.0/login-user', {
-      email: this.state.login.email,
-      password: this.state.login.password
+      email: login.email,
+      password: login.password
     })
       .then(resp => {
         console.log(resp.data.error === true)
@@ -86,88 +58,58 @@ export default class App extends React.Component {
           console.log('Not valid user or password')
           return false
         }
+        setToken({token: resp.data.token})
 
-        this.setState({
-          token: resp.data.token
-        })
-        this.authorization(resp.data.token)
+        // this.authorization(resp.data.token)
       })
       .catch(err => console.log(err))
   }
 
-  handleRegisterSubmit (event) {
-    event.preventDefault()
-    if (this.validate()) {
-      axios.post('/api/v1.0/register-user', {
-        email: this.state.register.email,
-        password: this.state.register.password
-      })
-        .then(resp => console.log(resp))
-        .catch(err => console.log(err))
-    }
-
-    console.log('validation', this.state.register.validation)
-  }
-
-  validate (event) {
+  function validate (e) {
     let valid = true
 
-    this.setState(prevState => ({
-      register: {
-        ...prevState.register,
-        validation: []
-      }
-    }))
+    setRegister((register) => ({ ...register, validation: [] }))
 
-    if (this.state.register.email === '') {
-      this.setState(prevState => ({
-        register: {
-          ...prevState.register,
-          validation: [...prevState.register.validation.concat(['E-mail is required'])]
-        }
-      }))
+    if (register.email === '') {
+      console.log('email vazio')
+      setRegister((register) => ({ ...register, validation: register.validation.concat('E-mail is required') }))
       valid = false
+      console.log(register)
     }
 
-    if (this.state.register.password === '') {
-      this.setState(prevState => ({
-        register: {
-          ...prevState.register,
-          validation: [...prevState.register.validation.concat(['Password is required'])]
-        }
-      }))
+    if (register.password === '') {
+      setRegister((register) => ({ ...register, validation: register.validation.concat('Password is required') }))
       valid = false
     }
 
     return valid
   }
 
-  render () {
-    return (
-      <Router>
-        {this.state.auth ? (
-          <Route exact path='/' render={() => <div>Logged</div>} />
-        )
-          : (
-            <Route
-              exact path='/'
-              render={() =>
-                <Login
-                  login={this.state.login}
-                  onHandleChange={this.handleChange}
-                  onHandleLoginSubmit={this.handleLoginSubmit}
-                />}
-            />
-          )}
-        <Route
-          path='/register' render={() =>
-            <Register
-              register={this.state.register}
-              onHandleChange={this.handleChange}
-              onHandleRegisterSubmit={this.handleRegisterSubmit}
-            />}
-        />
-      </Router>
-    )
-  }
+
+  return (
+    <Router>
+      {auth ? (
+        <Route exact path='/' render={() => <div>Logged</div>} />
+      )
+        : (
+          <Route
+            exact path='/'
+            render={() =>
+              <Login
+                login={login}
+                onHandleChange={handleChange}
+                onHandleLoginSubmit={handleLoginSubmit}
+              />}
+          />
+        )}
+      <Route
+        path='/register' render={() =>
+          <Register
+            register={register}
+            onHandleChange={handleChange}
+            onHandleRegisterSubmit={handleRegisterSubmit}
+          />}
+      />
+    </Router>
+  )
 }
